e062ae0c112c87ea8919bf106079706c25a3b511
sagar1sharma <sagarsharmazzz12@gmail.com>
2023-07-28 16:54:27 +0530

    second

--- 1/index.js
+++ 1/index.js
@@ -5,30 +5,40 @@ import data from "./data.js";
 
 dotenv.config();
 const app = express();
-const header = { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA1MzY1MTUsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiNDg3ZGJlNmEtZDczOC00YTYwLWEyZjUtMTZhODc0MWVmY2M3Iiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjExMzIwODAyNzIwIn0.aoNCUwUSf_cMJGVZC0u4yrDhsNJVSXl2ktdk-ZHX9DY' };
 
 app.get("/", async(req, res) => {
     const actualData = data;
     const currTime = new Date();
     const currInMin = (currTime.getHours() * 60) + currTime.getMinutes();
 
-    actualData.forEach(element => {
-        const trainAdjustedTime = (element.departureTime.Hours * 60) + element.departureTime.Minutes + element.delayedBy;
-        if(((currInMin + 720) % 1440) >= (trainAdjustedTime % 1440)){
-            res.send({
-                name: element.trainName,
-                number: element.trainNumber,
-                hours: element.departureTime.Hours,
-                minutes: element.departureTime.Minutes,
-                delay: element.delayedBy,
-                seats: element.seatsAvailable,
-                price: element.price
-            })
-        }
-    });
+    // actualData.map((element) => {
+    //     
+    //     res.send({
+    //         hour: element.departureTime.Hours,
+    //         minutes: element.departureTime.Minutes,
+    //         seconds: element.delayedBy
+    //     })
+    //     if((trainAdjustedTime - currInMin + 1440) % 1440 <= 720){
+    //         res.send({
+    //             name: element.trainName,
+    //             number: element.trainNumber,
+    //             hours: element.departureTime.Hours,
+    //             minutes: element.departureTime.Minutes,
+    //             delay: element.delayedBy,
+    //             seats: element.seatsAvailable,
+    //             price: element.price
+    //         })
+    //     }
+    // });
+
+    const validTrains = actualData.filter((ele) => {
+        const trainAdjustedTime = (ele.departureTime.Hours * 60) + ele.departureTime.Minutes + ele.delayedBy;
+        return ((trainAdjustedTime -currInMin + 1440) % 1440 <= 720);
+    })
+
+    res.json({trains: validTrains});
 })
 
 app.listen(process.env.PORT || 2000, async () => {
     console.log("Server Started");
-    console.log(data);
 })
