9637788db407b18d7709dc1eebe8b09a97794933
sagar1sharma <sagarsharmazzz12@gmail.com>
2023-07-28 17:03:00 +0530

    backend complete

--- 1/index.js
+++ 1/index.js
@@ -5,31 +5,25 @@ import data from "./data.js";
 
 dotenv.config();
 const app = express();
+const actualData = data;
 
+//for question 2, for the purpose of front end
+app.get("/all", (req, res) => {
+    res.json({trains: actualData});
+})
+
+//for question 2, for the purpose of front end
+app.get("/train/:no", (req, res) => {
+    const no = req.params.no;
+    res.json({trains: actualData.filter((ele) => {
+        return no == ele.trainNumber;
+    })})
+})
+
+//For question 1, returning all trains whose departure time is upto next 12 hours including delay time
 app.get("/", async(req, res) => {
-    const actualData = data;
     const currTime = new Date();
     const currInMin = (currTime.getHours() * 60) + currTime.getMinutes();
-
-    // actualData.map((element) => {
-    //     
-    //     res.send({
-    //         hour: element.departureTime.Hours,
-    //         minutes: element.departureTime.Minutes,
-    //         seconds: element.delayedBy
-    //     })
-    //     if((trainAdjustedTime - currInMin + 1440) % 1440 <= 720){
-    //         res.send({
-    //             name: element.trainName,
-    //             number: element.trainNumber,
-    //             hours: element.departureTime.Hours,
-    //             minutes: element.departureTime.Minutes,
-    //             delay: element.delayedBy,
-    //             seats: element.seatsAvailable,
-    //             price: element.price
-    //         })
-    //     }
-    // });
 
     const validTrains = actualData.filter((ele) => {
         const trainAdjustedTime = (ele.departureTime.Hours * 60) + ele.departureTime.Minutes + ele.delayedBy;
